4e4fd62a9adedfe8aee3bbd152b3d92482b8338d
gopika~ <gopikaks.cse2020@citchennai.net>
2023-11-19 23:03:30 +0530

    clevercloud

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 const db = mysql.createConnection({
-    host: "localhost",
-    database: "flightreservation",
-    user: "gopika",
-    password: "password",
+    host: "b7vt4l9sq4h6yjh8ld4u-mysql.services.clever-cloud.com",
+    database: "b7vt4l9sq4h6yjh8ld4u",
+    user: "urg3mkovbvlvdtn8",
+    password: "klNXyWBBiAHCtO7HbZwm",
 });
 module.exports = db
